3a9260fce1bdb0659c9e83d51c37bccaab028c7d
Bobby DeVeaux <bdeveaux@ukcloud.com>
2016-11-25 15:08:53 +0000

    adding nameservers

--- main.tf
+++ main.tf
@@ -7,36 +7,37 @@ resource "openstack_compute_keypair_v2" "test-keypair" {
 }
 
 resource "openstack_networking_network_v2" "example_network1" {
-  name = "example_network_1"
+  name           = "example_network_1"
   admin_state_up = "true"
 }
 
 resource "openstack_networking_subnet_v2" "example_subnet1" {
-  name = "example_subnet_1"
-  network_id = "${openstack_networking_network_v2.example_network1.id}"
-  cidr = "10.10.0.0/24"
-  ip_version = 4
+  name            = "example_subnet_1"
+  network_id      = "${openstack_networking_network_v2.example_network1.id}"
+  cidr            = "10.10.0.0/24"
+  ip_version      = 4
+  dns_nameservers = ["8.8.8.8", "8.8.4.4"]
 }
 
 resource "openstack_compute_secgroup_v2" "example_secgroup_1" {
   name = "example_secgroup_1"
   description = "an example security group"
   rule {
-    from_port = 22
-    to_port = 22
+    from_port   = 22
+    to_port     = 22
     ip_protocol = "tcp"
-    cidr = "0.0.0.0/0"
+    cidr        = "0.0.0.0/0"
   }
    rule {
-    from_port = 80
-    to_port = 80
+    from_port   = 80
+    to_port     = 80
     ip_protocol = "tcp"
-    cidr = "0.0.0.0/0"
+    cidr        = "0.0.0.0/0"
   }
 }
 
 resource "openstack_networking_router_v2" "example_router_1" {
-  name = "example_router1"
+  name             = "example_router1"
   external_gateway = "893a5b59-081a-4e3a-ac50-1e54e262c3fa"
 }
 
@@ -50,10 +51,10 @@ resource "openstack_networking_floatingip_v2" "example_floatip_1" {
 }
 
 resource "openstack_compute_instance_v2" "example_instance" {
-  name = "example_instance"
-  image_id = "8e892f81-2197-464a-9b6b-1a5045735f5d"
-  flavor_id = "c46be6d1-979d-4489-8ffe-e421a3c83fdd"
-  key_pair = "${openstack_compute_keypair_v2.test-keypair.name}"
+  name            = "example_instance"
+  image_id        = "8e892f81-2197-464a-9b6b-1a5045735f5d"
+  flavor_id       = "c46be6d1-979d-4489-8ffe-e421a3c83fdd"
+  key_pair        = "${openstack_compute_keypair_v2.test-keypair.name}"
   security_groups = ["${openstack_compute_secgroup_v2.example_secgroup_1.name}"]
 
   metadata {
@@ -61,7 +62,7 @@ resource "openstack_compute_instance_v2" "example_instance" {
   }
 
   network {
-    name = "${openstack_networking_network_v2.example_network1.name}"
+    name        = "${openstack_networking_network_v2.example_network1.name}"
     floating_ip = "${openstack_networking_floatingip_v2.example_floatip_1.address}"
   }
 }
